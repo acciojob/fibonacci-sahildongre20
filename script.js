c9a3428bf8956a1986c2567e8910d0a85da9fd6d
sahildongre20 <54145208+sahildongre20@users.noreply.github.com>
2023-11-17 19:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	if(num===1 || num==0){
+		return num;
+	}
+	else{
+		let num1 = 0;
+		let num1 = 1;
+
+		let fib =0;
+		for(let i=0;i<num;i++){
+			fib += num1+num2;
+			num1=num2;
+			num2=fib;
+		}
+		
+	}
 }
 
 module.exports = fibonacci;
